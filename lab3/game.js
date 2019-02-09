ef0a9ffdc3cca51487e3bfd6b48c49798ef83e59
michaelRAMAGE <47224690+michaelRAMAGE@users.noreply.github.com>
2019-02-08 21:42:50 -0500

    dfddf

--- lab3/game.js
+++ lab3/game.js
@@ -1,5 +1,70 @@
+
+var score = 0;
+var answerArr = ["Anananarivo", "Baghdad", "Caracas"];
+
+var answerQ = function(answer)
+{
+  var getElemId = document.getElementById("answer");
+  console.log(getElemId);
+  var inputQ = "";
+  var i;
+  for (i=0; i<getElemId.length; i++) {
+    inputQ += getElemId.elements[i].value;
+  }
+  document.getElementById("present").innerHTML=inputQ;
+
+  var result;
+  if (inputQ == answer) {
+    result = document.getElementById("result");
+    result.innerText="Correct";
+  }
+  else {
+    result = document.getElementById("result");
+    result.innerText="Incorrect";
+
+    }
+}
+
+var truth_b1 = getElementById("box1").getAttribute("disabled");
+var truth_b2 = getElementById("box2").getAttribute("disabled");
+var truth_b3 = getElementById("box3").getAttribute("disabled");
+if (truth_b1 && truth_b2 && truth_b3) {
+  setAttribute
+}
+}
+
+
 var clicked_1 = function()
 {
     var t = document.getElementById("box1");
-    t.setAttribute("disabled", "true")
+    t.setAttribute("disabled", "true");
+
+    var x = document.getElementById("question");
+    x.innerText="What is the capital of Madagascar?";
+
+    answerQ(answerArr[0]);
+
+
+}
+
+var clicked_2 = function()
+{
+    var t = document.getElementById("box2");
+    t.setAttribute("disabled", "true");
+    var x = document.getElementById("question");
+    x.innerText="What is the capital of Iraq?";
+    answerQ(answerArr[1]);
+
+
+}
+
+var clicked_3 = function()
+{
+    var t = document.getElementById("box3");
+    t.setAttribute("disabled", "true");
+    var x = document.getElementById("question");
+    x.innerText="";
+
+    answerQ();
+
 }
