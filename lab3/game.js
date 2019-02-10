bfd5bfc026959f0126df1ccd748e8f602e2b5451
michaelRAMAGE <47224690+michaelRAMAGE@users.noreply.github.com>
2019-02-10 01:31:23 -0500

    Fixed some bugs (cannot submit more than once. after image has been clicked, cannot be clicked again. after entering all answer, next page link opens.)
    other bugs:
    action attribute value not changing to new page

--- lab3/game.js
+++ lab3/game.js
@@ -1,15 +1,32 @@
 
-var score = 0;
-var extra = -1;
-var cap;
+// GLOBAL VARIABLES
+var score_one = 0;
+var score_two = 0;
+  // create a switch variable; after going to next page
+  // for player two, update a global variable to correspond
+  // to incremented index for score[]
+  // index 0 is player 1; index 1 is player 2.
+var p = 0;
+var score = [0,0]
+  // REAL ANSWER
 var answer = "";
-var answerArr = ["Canberra","Baghdad", "Caracas"];
-var everything = [{country: "Australia", capital: "Canberra", question: "Is the capital of Australia Melbourne, Canberra, or Sydney?"},
-{country: "Iraq", capital: "Baghdad", question: "What is the capital of Iraq?"},
-{country: "Venezuela", capital: "Caracas", question: "What is the capital of Venezuela?"}];
+  // LIST OF OBJECTS (ANSWER KEY)
+var permit = [false, false, false];
+var everything =
+[
+{capital: "Canberra", question: "Is the capital of Australia Melbourne, Canberra, or Sydney?"},
+{capital: "Baghdad", question: "What is the capital of Iraq?"},
+{capital: "Caracas", question: "What is the capital of Venezuela?"},
+{capital: "Ottawa", question: "Is the capital of Canada Toronto, Montreal, or Ottawa?"},
+{capital: "Cairo", question: "What is the capital of Egypt?"},
+{capital: "Lima", question: "What is the capital of Peru?"}];
 
+// THIS FUNCTION IS CALLED BY CLICKING SUBMIT ANSWER ON WEBSPAGE
+// ITS PURPOSE IS TO STORE INPUTS, COMPARE INPUTS TO ANSWER,
+// AND ALLOW FOR LEVEL INCREASE
 var answerQ = function()
 {
+  // Read and Store user Answer Input
   var getElemId = document.getElementById("answer");
   console.log(getElemId);
   var inputQ = "";
@@ -19,38 +36,47 @@ var answerQ = function()
   }
   document.getElementById("present").innerHTML=inputQ;
 
+// Compare user Answer Input to Answer
   var result;
   if (inputQ == answer) {
     result = document.getElementById("result");
     result.innerText="Correct";
-    score+=1;
-    extra+=1;
-    cap=score-extra;
-    score=score+cap;
-    document.getElementById("score").innerText="Score: "+cap;
+    score[p]+=1;
+    document.getElementById("submit").removeAttribute("disabled");
+    document.getElementById("submit").disabled=true;
+    document.getElementById("score").innerText="Player" + p+1 + ", Score: "+ score[p];
   }
-  else {
+  else if(inputQ==""){
     result = document.getElementById("result");
     result.innerText="Incorrect";
+    document.getElementById("present").innerText="Invalid Entry: Submitted without entering. Try again!";
+
     }
 
-  var truth_b1 = getElementById("box1").getAttribute("disabled");
-  var truth_b2 = getElementById("box2").getAttribute("disabled");
-  var truth_b3 = getElementById("box3").getAttribute("disabled");
-  if (truth_b1 && truth_b2 && truth_b3) {
+  // Open next level after all boxes have been clicked
+  // PROBLEM: Cannot set disabled attributes, so cannot check this way
+  var truth_b1 = document.getElementById("box1").getAttribute("disabled");
+  var truth_b2 = document.getElementById("box2").getAttribute("disabled");
+  var truth_b3 = document.getElementById("box3").getAttribute("disabled");
+  if (permit[0] && permit[1] && permit[2]) {
     document.getElementById("procede").setAttribute("action", "second.html");
     document.getElementById("next").removeAttribute("disabled");
+    p=1;
   }
 }
 
 
 var clicked_1 = function()
 {
+    document.getElementById("submit").removeAttribute("disabled");
     document.getElementById("result").innerText="";
     document.getElementById("present").innerText="";
 
     var t = document.getElementById("box1");
-    t.setAttribute("disabled", true);
+    t.setAttribute("disabled","");
+    t.disabled=true;
+    permit[0]=true;
+
 
     var x = document.getElementById("question");
     x.innerText=everything[0].question;
@@ -59,11 +85,16 @@ var clicked_1 = function()
 
 var clicked_2 = function()
 {
+    document.getElementById("submit").removeAttribute("disabled");
     document.getElementById("result").innerText="";
     document.getElementById("present").innerText="";
 
     var t = document.getElementById("box2");
-    t.setAttribute("disabled", true);
+    t.disabled=true;
+    permit[1]=true;
+
+
+
     var x = document.getElementById("question");
     x.innerText=everything[1].question;
     answer = everything[1].capital;
@@ -71,11 +102,14 @@ var clicked_2 = function()
 
 var clicked_3 = function()
 {
+    document.getElementById("submit").removeAttribute("disabled");
     document.getElementById("result").innerText="";
     document.getElementById("present").innerText="";
 
     var t = document.getElementById("box3");
-    t.setAttribute("disabled", true);
+    t.disabled=true;
+    permit[2]=true;
+
     var x = document.getElementById("question");
     x.innerText=everything[2].question;
     answer = everything[2].capital;
